b0da03052747410f32f0f768e16da1b61bbb1227
Steffen Krutzsch <steffenkrut@gmail.com>
2026-03-30 09:40:53 +0200

    refactor: create new file for Login Page

---
+++ flutter_homework/lib/pages/login_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: SizedBox(
+          height: 400,
+          width: 1000,
+          child: Column(
+            children: [
+              Expanded(flex: 5, child: Container(color: Colors.white)),
+              Expanded(
+                flex: 5,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "Login: ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter your username',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(flex: 5, child: Container(color: Colors.purple)),
+              Expanded(flex: 5, child: Container(color: Colors.green)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
