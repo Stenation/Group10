f8db5dd92df5b69ac45429fb797cbc32fbaca089
elox5 <elox55555@gmail.com>
2026-04-23 20:31:48 +0300

    Update menu items to include image

--- 03-restaurant-app/lib/data/menu_data.dart
+++ 03-restaurant-app/lib/data/menu_data.dart
@@ -10,16 +10,19 @@ const List<MenuItem> starterItems = [
     name: 'Flutter fries',
     description: "Fluttiest fries around. Fresh potatoes.",
     price: 5.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
   MenuItem(
     name: 'Backend bread',
     description: "Fluffy, fluttery bread, yummy.",
     price: 3.5,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
   MenuItem(
     name: 'Mozzarella sticks of ram',
     description: "Cheesy deliciousness",
     price: 4.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
 ];
 
@@ -28,16 +31,19 @@ const List<MenuItem> mainCourseItems = [
     name: 'Pi pizza',
     description: "Beautiful, round, pizza",
     price: 10.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
   MenuItem(
     name: 'Rubber duck wrap ',
     description: "Wrapped up duck. Helps with debugging",
     price: 8.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
   MenuItem(
     name: 'Barbe-queue burger',
     description: "One at time, it's that popular",
     price: 9.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
 ];
 
@@ -46,18 +52,35 @@ const List<MenuItem> dessertItems = [
     name: 'Ice cream with bits and bites',
     description: "Smooth, velvety, delicious",
     price: 5.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
   MenuItem(
     name: 'Raspberry pi',
     description: "Versatile, little piece of deliciousness",
     price: 6.0,
+    imagePath: 'assets/images/menu/item.jpg',
   ),
 ];
 
 const List<MenuItem> drinkItems = [
-  MenuItem(name: 'Water', description: "If you're boring", price: 2.0),
-  MenuItem(name: 'Wine', description: "Cherry wine", price: 6.0),
-  MenuItem(name: 'Beer', description: "Karhu", price: 5.0),
+  MenuItem(
+    name: 'Water',
+    description: "If you're boring",
+    price: 2.0,
+    imagePath: 'assets/images/menu/item.jpg',
+  ),
+  MenuItem(
+    name: 'Wine',
+    description: "Cherry wine",
+    price: 6.0,
+    imagePath: 'assets/images/menu/item.jpg',
+  ),
+  MenuItem(
+    name: 'Beer',
+    description: "Karhu",
+    price: 5.0,
+    imagePath: 'assets/images/menu/item.jpg',
+  ),
 ];
 
 const List<MenuCategory> menuCategories = [
--- 03-restaurant-app/lib/models/menu_item.dart
+++ 03-restaurant-app/lib/models/menu_item.dart
@@ -2,10 +2,12 @@ class MenuItem {
   final String name;
   final String description;
   final double price;
+  final String imagePath;
 
   const MenuItem({
     required this.name,
     required this.description,
     required this.price,
+    required this.imagePath,
   });
 }
